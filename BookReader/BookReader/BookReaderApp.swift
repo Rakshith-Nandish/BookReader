586814e476b1972a7a3b79e1c75cd5f7424ddd62
Rakshith Nandish <rakshith.nandish@ocado.com>
2025-05-17 23:48:07 +0100

    first commit

---
+++ BookReader/BookReader/BookReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookReaderApp.swift
+//  BookReader
+//
+//  Created by Rakshith Nandish on 17/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct BookReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookReader/BookReaderTests/BookReaderTests.swift
@@ -0,0 +1,16 @@
+//
+//  BookReaderTests.swift
+//  BookReaderTests
+//
+//  Created by Rakshith Nandish on 17/05/2025.
+//
+
+import Testing
+
+struct BookReaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
